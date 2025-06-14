6552a2e1d1490a7c81ec27d42a55d5548b9a9570
itscsp <chethanspoojary1@gmail.com>
2025-06-14 16:30:52 +0530

    Building search gutenburge component

---
+++ build/video-search/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'e368e9a2964047c07e4e');
---
+++ build/video-search/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '7adfea8d4a12e0062226');
